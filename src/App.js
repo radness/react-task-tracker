8daa0e46fa7468533f620c05512daf7901fbcd48
radness <yhs5128@gmail.com>
2021-09-25 15:39:42 +0900

    add json server

--- src/App.js
+++ src/App.js
@@ -2,37 +2,21 @@ import Header from './components/Header'
 import Tasks from './components/Tasks'
 import { useState } from 'react'
 
-function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Meeting at School',
-      day: 'Feb 6th at 1:30pm',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Food Shopping',
-      day: 'Feb 7th at 3:30pm',
-      reminder: false,
-    },
-  ])
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
 
   // Delete Task
   const deleteTask = (id) => {
-    console.log('delete', id);
+    setTasks(tasks.filter())
   }
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      <Tasks tasks={tasks}
+        onDelete={deleteTask}
+      />
     </div>
   );
 }
